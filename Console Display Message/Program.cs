7b86b52ac9558e671dbacdb66c21f327213b7019
GriffinG1 <4n0nym0ush4ck3r666@gmail.com>
2019-03-31 12:58:07 -0600

    Add project files.

---
+++ Console Display Message/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome!"); // Greets the user
+            Console.WriteLine("Press \"enter\" to close the program."); // Advises the user on how to properly exit the program
+            Console.ReadLine(); // Allows input and waits for Enter to be pressed
+        }
+    }
+}
