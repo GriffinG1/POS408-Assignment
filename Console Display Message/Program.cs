a4f260a3a1321dcb0920440565e35d753f08badf
GriffinG1 <4n0nym0ush4ck3r666@gmail.com>
2019-03-31 13:10:06 -0600

    Finish Week 2 Assignment

--- Console Display Message/Program.cs
+++ Console Display Message/Program.cs
@@ -11,6 +11,17 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome!"); // Greets the user
+            Console.WriteLine("What's your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What's your zip code?");
+            int zipCode = int.Parse(Console.ReadLine());
+            Console.WriteLine("What's your gross monthly pay?");
+            int grossPay = int.Parse(Console.ReadLine());
+            double taxes = Convert.ToDouble(grossPay) * 0.07;
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Zip code: {zipCode}");
+            Console.WriteLine($"Gross monthly pay: {grossPay}");
+            Console.WriteLine($"Monthly taxes: {taxes}");
             Console.WriteLine("Press \"enter\" to close the program."); // Advises the user on how to properly exit the program
             Console.ReadLine(); // Allows input and waits for Enter to be pressed
         }
