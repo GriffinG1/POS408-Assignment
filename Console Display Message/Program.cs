15271f3e0cad39e0cfb4a0f9bd3d4e59bdc50135
GriffinG1 <4n0nym0ush4ck3r666@gmail.com>
2019-04-03 16:12:50 -0600

    Remove redundancy at professor request

--- Console Display Message/Program.cs
+++ Console Display Message/Program.cs
@@ -13,25 +13,10 @@ namespace ConsoleApp1
             Console.WriteLine("Welcome!"); 
             Console.WriteLine("What's your name?"); 
             string name = Console.ReadLine();
-            while (name == "") // forces users to input a name
-            {
-                Console.WriteLine("I'm sorry, I didn't get that. What's your name?");
-                name = Console.ReadLine();
-            }
             Console.WriteLine("What's your zip code?"); 
-            bool zipNumeric = int.TryParse(Console.ReadLine(), out int zipCode); // Takes user's zip code, attempts to convert it to int, and assigns it to int zipCode if successful
-            while (!zipNumeric || zipCode < 10000 || zipCode > 99999) // Forces users to input an integer
-            {
-                Console.WriteLine("Your zip code must be a five-digit integer. What's your zip code?");
-                zipNumeric = int.TryParse(Console.ReadLine(), out zipCode);
-            }
+            int zipCode = int.Parse(Console.ReadLine()); // Takes user's zip code, attempts to convert it to int, and assigns it to int zipCode if successful
             Console.WriteLine("What's your gross monthly pay?");
-            bool payNumeric = int.TryParse(Console.ReadLine(), out int grossPay); // Takes user's gross monthly pay, attempts to convert it to int, and assigns it to int grossPay if successful
-            while (!payNumeric) // Forces users to input an integer
-            {
-                Console.WriteLine("Your pay must be an integer. What's your gross monthly pay?");
-                payNumeric = int.TryParse(Console.ReadLine(), out grossPay);
-            }
+            int grossPay = int.Parse(Console.ReadLine()); // Takes user's gross monthly pay, attempts to convert it to int, and assigns it to int grossPay if successful
             double taxes = Convert.ToDouble(grossPay) * 0.07; // Computes user's monthly taxes by taking 7% of user's gross monthly pay
             Console.WriteLine($"Name: {name}"); 
             Console.WriteLine($"Zip code: {zipCode}"); 
