f34713fa6ad36af1961c8945f87a3f6c83fdbef3
GriffinG1 <4n0nym0ush4ck3r666@gmail.com>
2019-04-08 14:42:41 -0600

    Final cleanup

--- Console Display Message/Program.cs
+++ Console Display Message/Program.cs
@@ -16,14 +16,14 @@ namespace ConsoleApp1
             Console.WriteLine("What's your zip code?"); 
             string zipCode = Console.ReadLine();
             Console.WriteLine("What's your gross monthly pay?");
-            double grossPay = double.Parse(Console.ReadLine()); // Takes user's gross monthly pay, attempts to convert it to double, and assigns it to grossPay if successful
-            double taxes = grossPay * 0.07; // Computes user's monthly taxes by taking 7% of user's gross monthly pay
+            Console.Write("$");
+            string grossPay = Console.ReadLine();
+            double taxes = System.Math.Round(double.Parse(grossPay) * 0.07, 2); // Converts grossPay to double, computes user's monthly taxes by taking 7%, and rounds it to two digits
             Console.WriteLine($"Name: {name}"); 
             Console.WriteLine($"Zip code: {zipCode}"); 
-            Console.WriteLine($"Gross monthly pay: {grossPay}");
-            Console.WriteLine($"Monthly taxes: {taxes}"); 
-            Console.WriteLine("Press the enter key to close the program."); 
-            Console.ReadLine(); // Allows input and waits for Enter to be pressed
+            Console.WriteLine($"Gross monthly pay: ${grossPay}");
+            Console.WriteLine($"Monthly taxes: ${taxes}"); 
+            Console.WriteLine("Press the enter key to close the program.");
         }
     }
 }
