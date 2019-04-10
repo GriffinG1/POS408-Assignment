90da5df83fb62b553491f1011c13b2472308b0ea
GriffinG1 <4n0nym0ush4ck3r666@gmail.com>
2019-04-09 18:38:17 -0600

    Cleanup and comments

--- Console Display Message/Program.cs
+++ Console Display Message/Program.cs
@@ -10,24 +10,32 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            // Sets total devs for the program to use.
             int TOTAL_DEVS = 3;
 
-            List<SoftwareDevelopers> devs = new List<SoftwareDevelopers>();
-            for (int i = 1; i <= TOTAL_DEVS; i++)
+            Console.WriteLine("Welcome!\n");
+
+            List<SoftwareDeveloper> devs = new List<SoftwareDeveloper>();
+            for (int i = 1; i <= TOTAL_DEVS; i++) // Fills the List devs with a TOTAL_DEVS amount of SoftwareDeveloper objects, while taking inputs for them
             {
-                SoftwareDevelopers dev = new SoftwareDevelopers();
+                SoftwareDeveloper dev = new SoftwareDeveloper();
                 Console.WriteLine($"Name for dev #{i}:");
                 dev.SetName(Console.ReadLine());
                 Console.WriteLine($"Zip code for dev #{i}:");
                 dev.SetZipCode(Console.ReadLine());
                 Console.WriteLine($"Gross monthly pay for dev #{i}:");
                 Console.Write("$");
-                dev.SetPay(decimal.Parse(Console.ReadLine()));
+                decimal grossPay;
+                bool hasPay = decimal.TryParse(Console.ReadLine(), out grossPay); // Prevents a crash if user inputs no data
+                if (hasPay)
+                {
+                    dev.SetPay(grossPay);
+                }
                 devs.Add(dev);
                 Console.WriteLine();
                 
             }
-            foreach (SoftwareDevelopers dev in devs)
+            foreach (SoftwareDeveloper dev in devs)
             {
                 dev.PrintData();
             }
@@ -37,7 +45,7 @@ namespace ConsoleApp1
         }
     }
 
-    class SoftwareDevelopers
+    class SoftwareDeveloper
     {
         private string name;
         private string zipCode;
@@ -45,9 +53,9 @@ namespace ConsoleApp1
         private decimal taxes;
         private static decimal taxRate = 0.07M;
 
-        public SoftwareDevelopers() {}
+        public SoftwareDeveloper() { }
 
-        public SoftwareDevelopers(string name, string zipCode, decimal grossPay)
+        public SoftwareDeveloper(string name, string zipCode, decimal grossPay)
         {
             this.name = name;
             this.zipCode = zipCode;
@@ -67,31 +75,45 @@ namespace ConsoleApp1
 
         public string GetPay()
         {
-            return string.Format("{0:C}", grossPay);
+            return string.Format("{0:C}", grossPay); // returns grossPay as a currency
         }
 
         public string GetTaxes()
         {
-            return string.Format("{0:C}", taxes);
+            return string.Format("{0:C}", taxes); // returns taxes as a currency
         }
 
         public void SetName(string name)
         {
-            this.name = name;
+            if (name == "") // Allows clean output if no data is set
+            {
+                this.name = "N/A";
+            }
+            else
+            {
+                this.name = name;
+            }
         }
 
         public void SetZipCode(string zipCode)
         {
-            this.zipCode = zipCode;
+            if (zipCode == "") // Allows clean output if no data is set
+            {
+                this.zipCode = "N/A";
+            }
+            else
+            {
+                this.zipCode = zipCode;
+            }
         }
 
         public void SetPay(decimal grossPay)
         {
             this.grossPay = grossPay;
-            taxes = Decimal.Multiply(grossPay, taxRate);
+            taxes = Decimal.Multiply(grossPay, taxRate); // Multiplies grossPay by the static decimal taxRate to get the dev's monthly taxes
         }
 
-        public void PrintData()
+        public void PrintData() // Outputs all the data in a clean block
         {
             Console.WriteLine($"Name: {GetName()}");
             Console.WriteLine($"Zip code: {GetZipCode()}");
