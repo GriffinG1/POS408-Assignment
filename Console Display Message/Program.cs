25e1d7ec4c1444d99786c8415848acc365fba66c
GriffinG1 <4n0nym0ush4ck3r666@gmail.com>
2019-04-09 18:01:12 -0600

    Expand to three devs for week 3

--- Console Display Message/Program.cs
+++ Console Display Message/Program.cs
@@ -10,21 +10,93 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome!"); 
-            Console.WriteLine("What's your name?"); 
-            string name = Console.ReadLine();
-            Console.WriteLine("What's your zip code?"); 
-            string zipCode = Console.ReadLine();
-            Console.WriteLine("What's your gross monthly pay?");
-            Console.Write("$");
-            string grossPay = Console.ReadLine();
-            double taxes = System.Math.Round(double.Parse(grossPay) * 0.07, 2); // Converts grossPay to double, computes user's monthly taxes by taking 7%, and rounds it to two digits
-            Console.WriteLine($"Name: {name}"); 
-            Console.WriteLine($"Zip code: {zipCode}"); 
-            Console.WriteLine($"Gross monthly pay: ${grossPay}");
-            Console.WriteLine($"Monthly taxes: ${taxes}"); 
+            int TOTAL_DEVS = 3;
+
+            List<SoftwareDevelopers> devs = new List<SoftwareDevelopers>();
+            for (int i = 1; i <= TOTAL_DEVS; i++)
+            {
+                SoftwareDevelopers dev = new SoftwareDevelopers();
+                Console.WriteLine($"Name for dev #{i}:");
+                dev.SetName(Console.ReadLine());
+                Console.WriteLine($"Zip code for dev #{i}:");
+                dev.SetZipCode(Console.ReadLine());
+                Console.WriteLine($"Gross monthly pay for dev #{i}:");
+                Console.Write("$");
+                dev.SetPay(decimal.Parse(Console.ReadLine()));
+                devs.Add(dev);
+                Console.WriteLine();
+                
+            }
+            foreach (SoftwareDevelopers dev in devs)
+            {
+                dev.PrintData();
+            }
+            
             Console.WriteLine("Press the enter key to close the program.");
             Console.ReadLine();
+        }
+    }
+
+    class SoftwareDevelopers
+    {
+        private string name;
+        private string zipCode;
+        private decimal grossPay;
+        private decimal taxes;
+        private static decimal taxRate = 0.07M;
+
+        public SoftwareDevelopers() {}
+
+        public SoftwareDevelopers(string name, string zipCode, decimal grossPay)
+        {
+            this.name = name;
+            this.zipCode = zipCode;
+            this.grossPay = grossPay;
+            taxes = Decimal.Multiply(grossPay, taxRate);
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public string GetZipCode()
+        {
+            return zipCode;
+        }
+
+        public string GetPay()
+        {
+            return string.Format("{0:C}", grossPay);
+        }
+
+        public string GetTaxes()
+        {
+            return string.Format("{0:C}", taxes);
+        }
+
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+
+        public void SetZipCode(string zipCode)
+        {
+            this.zipCode = zipCode;
+        }
+
+        public void SetPay(decimal grossPay)
+        {
+            this.grossPay = grossPay;
+            taxes = Decimal.Multiply(grossPay, taxRate);
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine($"Name: {GetName()}");
+            Console.WriteLine($"Zip code: {GetZipCode()}");
+            Console.WriteLine($"Gross monthly pay: {GetPay()}");
+            Console.WriteLine($"Monthly taxes: {GetTaxes()}\n");
         }
     }
 }
